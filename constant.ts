ee0d01f22cec289c4ec9f7e0f253a18a2fac20fa
Aik-202 <ikogweassurance85@gmail.com>
2026-09-05 07:18:40 +0100

    feat: add static page content constants

---
+++ constant.ts
@@ -0,0 +1,98 @@
+export const navList = [
+    {
+        text: "About",
+        link: "/"
+    },
+    {
+        text: "What We Do",
+        link: "/"
+    },
+    {
+        text: "Jobs",
+        link: "/"
+    },
+    {
+        text: "Project",
+        link: "/"
+    },
+    {
+        text: "TG Academy",
+        link: "/"
+    },
+    {
+        text: "Strategic Partnership",
+        link: "/"
+    },
+    {
+        text: "Pricing",
+        link: "/"
+    },
+    {
+        text: "Book a Consultation",
+        link: "/"
+    },
+]
+
+export const lmsCourses = ["Business Analysis", "Design Thinking", "Effective Communication", "Entrepreneurship", "Career Development", "Business Model"]
+
+export const corporateTrainingPrograms = ["Leadership Training", "Strategic Planning and Implementation", "Project Management", "Sustainability Training", "Customised Training"]
+
+export const personalizedIndividualTrainingPrograms = ["Leadership Development", "Soft Skills Development", "Industry Specific Knowledge", "Technical Skills Enhancement", "Time Management and Productivity", "Career Development"]
+
+export const capacityDevPrograms = ["Tailored Training Programs", "Expert-Led Workshops", "Personalized Mentorship", "Technical Skills Enhancement", "Collaborative Learning Environment", "Ongoing Support and Resources"]
+
+export const managementDevPrograms = ["Enhanced Leadership Skills", "Improved Employee Engagement", "Stronger Organisational Culture", "Sustainable Growth"]
+
+export const ceoPrograms = ["Strategic Career Guidance", "Leadership Development", "CV Development", "Sustainability Leadership", "Communication Skills", "Business Model"]
+
+export const consultantPrograms = [
+    {
+        heading: "Expert-Led Learning",
+        text: "Gain insight from seasoned professionals in the field as they mentor you through the subtleties of business analysis."
+    },
+    {
+        heading: "Interactive Workshops",
+        text: "Engage in hands-on workshops designed to enhance your training capabilities and provide practical insights."
+    },
+    {
+        heading: "Comprehensive Curriculum",
+        text: "Access a robust curriculum that covers fundamental principles and advanced methodologies, ensuring a well-rounded understanding."
+    },
+    {
+        heading: "Global Recognition",
+        text: "You will attain a globally recognized certification, opening doors to new career opportunities and industry recognition."
+    }
+]
+
+export const testimonials = [
+    {
+        profilePic: "/images/aisha-yusuf.png",
+        name: "Aisha Yusuf",
+        position: "Founder, CraftHub NG",
+        comment: "Working with Tobams Group on our website was a breeze. They understood our vision and transformed it into a beautiful online space. Highly recommend their Website Design service!"
+    },
+    {
+        profilePic: "/images/john-davies.png",
+        name: "John Davies",
+        position: "Marketing Manager, E-Commerce Emporium",
+        comment: "Tobams Group's Digital Marketing strategies gave our brand the boost it needed. Simple yet powerful techniques that delivered tangible results. A pleasure to collaborate with!"
+    },
+    {
+        profilePic: "/images/chinonso-nwankwo.png",
+        name: "Chinonso Nwankwo",
+        position: "HR Director, FutureTech Solutions",
+        comment: "Tobams Group has been instrumental in our talent acquisition journey. Their Tech Talent Solution service consistently connects us with the right professionals. Reliable and straightforward."
+    },
+    {
+        profilePic: "/images/rita-ahmed.jpg",
+        name: "Rita Ahmed",
+        position: "Creative Director, ArtInnovate Studios",
+        comment: "The Creative and Digital Media services at Tobams Group brought our ideas to life. Simple, creative, and impactful – exactly what we needed for our projects."
+    }
+]
+
+export const whatWeDo = ["Sustainability Services", "Strategy Planning and Implementation", "Tech Talent Solutions", "Training and Development", "IT Consulting Services", "Strategic Partnership", "Social Impact", "Digital Marketing", "Talent Recruitment"]
+
+export const company = ["About", "Jobs", "Projects", "Our Founder", "Business Model", "The Team", "Contact Us", "Blog", "FAQs", "Help a Tech Talent", "Testimonials"]
+
+export const solution = ["Tobams Group Academy", "Help a Tech Talent", "Campus Ambassadors Program", "Join Our Platform", "Pricing", "Strategic Partnership", "Book a Consultation", "Digital Marketing", "Join Our Slack Community"]
